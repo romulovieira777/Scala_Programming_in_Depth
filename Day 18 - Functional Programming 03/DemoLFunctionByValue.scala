2544af547260cd08b5917f0ffd6390182e50e0b4
Romulo Vieira <romulo.vieira777@gmail.com>
2023-08-07 18:05:27 -0300

    Functional Programming 03

--- Day 18 - Functional Programming 03/DemoLFunctionByValue.scala
+++ Day 18 - Functional Programming 03/DemoLFunctionByValue.scala
@@ -1,6 +1,6 @@
 object DemoLFunctionByValue {
     def main(args: Array[String]): Unit = {
-        println("Main Function: " + exec(time()))
+        println("Main Function: " + exec(time(140698097570299)))
     }
 
     def time() : Long = {
@@ -11,12 +11,12 @@ object DemoLFunctionByValue {
     def exec(t:Long): Long = {
         println("Inside exec function")
         println("Time: " + t)
-        println("Exiting from time function)
+        println("Exiting from exec function)
         return t
     }
 }
 
 // Points to remember
 // 1. It will first execute the inside function "time()" and then execute the outside function "exec(140698097570299)"
-// 2. Since inside function "time ()"got executed first, it calls outside function "exec(140698097570299)" using the
+// 2. Since inside function "time ()" got executed first, it calls outside function "exec(140698097570299)" using the
 // value 140698097570299, it is called as "Function by Value"
--- Day 18 - Functional Programming 03/DemoMFunctionByName.scala
+++ Day 18 - Functional Programming 03/DemoMFunctionByName.scala
@@ -11,13 +11,13 @@ object DemoMFunctionByName {
     def exec(t: => Long): Long = {
         println("Entering exec function")
         println("Time: " + t)
-        println("Exiting from time function)
+        println("Exiting from exec function)
         return t
     }
 }
 
 // Points to remember
-// 1. It will first execute the OUTSIDE function "exec()" and then execute the outside function "exec(140698097570299)"
-// function "time()" every time it is being referred.
+// 1. It will first execute the OUTSIDE function "exec()" and then execute the INSIDEfunction "time()" every time it is
+// being referred.
 // 2. Since OUTSIDE function "exec ()" got executed first, it calls INSIDE function "time()" using the REFERENCED
-// VATIABLE T, it is called as "Function by Value"
+// VARIABLE T, it is called as "Function by Name"
